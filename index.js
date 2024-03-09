fe10da8046c22d43b2ac5655b866e1b96c71eafa
Madhura Ingole <121611425+madhura-ingole@users.noreply.github.com>
2024-03-09 16:21:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,11 +26,6 @@ document.addEventListener("keypress", function(event) {
 function makeSound(key) {
 
   switch (key) {
-    case "w":
-      var tom1 = new Audio("tom-1.mp3");
-      tom1.play();
-      break;
-
     case "a":
       var tom2 = new Audio("tom-2.mp3");
       tom2.play();
